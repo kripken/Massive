993a78940c0504e4e9262e56287b06ea69e3dcee
Alon Zakai <alonzakai@gmail.com>
2014-05-21 12:22:06 -0700

    add shell benchmark for lua-binarytrees

---
+++ lua/benchmark-shell.js
@@ -0,0 +1,25 @@
+
+var Module = {
+  printBuffer: '',
+  print: function(x) {
+    Module.printBuffer += x + '\n';
+  }
+};
+
+var pre = Date.now();
+
+var Module = { print: function(x) { Module.printBuffer += x + '\n' }, printBuffer: '' };
+var console = { log: function(){} };
+
+var startup = Date.now();
+load('lua.vm.js');
+startup = Date.now() - startup;
+
+function doIt(code) {
+  var start = Date.now();
+  Lua.execute(code);
+  print(['runtime:', Date.now() - start, '  startup:', startup]);
+}
+
+doIt(read('binarytrees.lua'));
+
